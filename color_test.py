6ea652c31d7f5cd6d48d8a12a175eaa3b366ee7a
Ulises Martinez <ulimartinez96@gmail.com>
2019-12-17 14:43:04 -0700

    added clock

--- color_test.py
+++ color_test.py
@@ -1,15 +1,49 @@
-
-
+from PIL import Image, ImageDraw, ImageFont
+from datetime import datetime
+from time import sleep
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port = 8080
-byt = '33333333'.encode()
-s.connect(('192.168.2.97', port))
-for i in range(50):
-    msg = '33333333'
-    num = int(msg, 16)
-    byt += msg.encode('utf-8')
-print(byt)
-s.send(byt)
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = 8080
+    s.connect(('192.168.2.4', port))
+    
+    while True:
+        now = datetime.now()
+        hour = now.strftime('%H')
+        minu = now.strftime('%M')
+        img = Image.new('RGB', (20, 25), color = 'red')
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype('/usr/share/fonts/TTF/Koruri-Bold.ttf', size = 12)
+        color = 'rgb(0, 255, 0)'
+        draw.text((3, -3), hour, fill=color, font=font)
+        draw.text((3, 10), minu, fill=color, font=font)
+        s.sendall(image_to_bytearray(img))
+        sleep(5)
 
+def image_to_bytearray(image):
+    byt = None
+    width, height = image.size
+    for x in range(width):
+        if x % 2 == 0:
+            for y in range(height-1, -1, -1):
+                r, g, b = image.getpixel((x, y))
+                pixelstr= '%2X%2X%2X00' % (g, r, b)
+                pixelstr= pixelstr.replace(' ', '0')
+                if byt is None:
+                    byt = bytearray.fromhex(pixelstr)
+                else:
+                    byt += bytearray.fromhex(pixelstr)
+        else:
+            for y in range(height):
+                r, g, b = image.getpixel((x, y))
+                pixelstr= '%2X%2X%2X00' % (g, r, b)
+                pixelstr= pixelstr.replace(' ', '0')
+                if byt is None:
+                    byt = bytearray.fromhex(pixelstr)
+                else:
+                    byt += bytearray.fromhex(pixelstr)
+    return byt
+           
 
+if __name__ == "__main__":
+    main()
