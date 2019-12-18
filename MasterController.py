03dd449eff198a4780e0a1762e0bcde46e53da7f
Ulises Martinez <ulimartinez96@gmail.com>
2019-12-18 10:57:20 -0700

    added master class

--- MasterController.py
+++ MasterController.py
@@ -1,11 +1,29 @@
 from .MqttCli import MqttCli
+from .clock import Clock
+from .pixelutils import PixelUtils
+from time import sleep
+import socket
 
 class MasterController:
-    def __init__(self, clock, mqtt):
+    def __init__(self, clock, mqtt, host, port):
         self.clock = clock
         self.mqtt = mqtt
+        self.host = host
+        self.port = port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def run(self):
+        self.mqtt.connect()
+        self.s.connect((self.host, self.port))
+        while True:
+            img = self.clock.getImage()
+            s.sendall(PixelUtils.image_to_bytearray(img))
+            sleep(0.5)
        
 def main():
     mqtt = MqttCli('mqtt.gbridge.io', 8883)
+    clock = Clock()
+    master = MasterController(clock, mqtt, 'localhost', 8080)
+
 if __name__ == '__main__':
     main()
